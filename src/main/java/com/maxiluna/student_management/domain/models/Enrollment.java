968fb2f6e27ce47f79d2c20c7f28a691358b3c9f
Maximiliano Luna <maxiespn21@gmail.com>
2025-01-31 22:15:16 -0300

    feat(domain): add Enrollment entity with student and subject relationships
    - Create Enrollment class extending SoftDeleteEntity.
    - Add attributes: id, student, subject, and studentStatus.
    - Establish relationships with Student and Subject entities.

---
+++ src/main/java/com/maxiluna/student_management/domain/models/Enrollment.java
@@ -0,0 +1,17 @@
+package com.maxiluna.student_management.domain.models;
+
+import com.maxiluna.student_management.domain.enums.StudentStatus;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@EqualsAndHashCode(callSuper = true)
+public class Enrollment extends SoftDeleteEntity {
+    private Long id;
+    private Student student;
+    private Subject subject;
+    private StudentStatus studentStatus;
+}
