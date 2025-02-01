2d94ede59a25918e1f479998db7f40f8f447f36e
Maximiliano Luna <maxiespn21@gmail.com>
2025-01-31 22:16:01 -0300

    feat(domain): add Attendance entity with student and class record relationships
    - Create Attendance class extending SoftDeleteEntity.
    - Add attributes: id, dateRecord, student, classRecord, and attendanceStatus.
    - Establish relationships with Student and ClassRecord entities.

---
+++ src/main/java/com/maxiluna/student_management/domain/models/Attendance.java
@@ -0,0 +1,20 @@
+package com.maxiluna.student_management.domain.models;
+
+import com.maxiluna.student_management.domain.enums.AttendanceStatus;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@EqualsAndHashCode(callSuper = true)
+public class Attendance extends SoftDeleteEntity {
+    private Long id;
+    private LocalDate dateRecord;
+    private Student student;
+    private ClassRecord classRecord;
+    private AttendanceStatus attendanceStatus;
+}
