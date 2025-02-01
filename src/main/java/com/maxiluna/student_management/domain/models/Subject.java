14f2b26c6b810825d96e686cd529203c5b4fe466
Maximiliano Luna <maxiespn21@gmail.com>
2025-01-31 22:08:12 -0300

    feat(domain): add Subject entity with relationships
    - Extend SoftDeleteEntity to create Subject class.
    - Add attributes: id, name, academicYear, career, teacher.
    - Establish relationships with Career and User entities.

---
+++ src/main/java/com/maxiluna/student_management/domain/models/Subject.java
@@ -0,0 +1,17 @@
+package com.maxiluna.student_management.domain.models;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@EqualsAndHashCode(callSuper = true)
+public class Subject extends SoftDeleteEntity {
+    private Long id;
+    private String name;
+    private Integer academicYear;
+    private Career career;
+    private User teacher;
+}
