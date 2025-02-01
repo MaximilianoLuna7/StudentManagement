0a1eaee64eaff4ab114e435e10a9577be9ef6d18
Maximiliano Luna <maxiespn21@gmail.com>
2025-01-31 22:15:45 -0300

    feat(domain): add ClassRecord entity with subject relationship
    - Create ClassRecord class extending SoftDeleteEntity.
    - Add attributes: id, topic, activities, date, and subject.
    - Establish relationship with Subject entity.

---
+++ src/main/java/com/maxiluna/student_management/domain/models/ClassRecord.java
@@ -0,0 +1,19 @@
+package com.maxiluna.student_management.domain.models;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@EqualsAndHashCode(callSuper = true)
+public class ClassRecord extends SoftDeleteEntity {
+    private Long id;
+    private String topic;
+    private String activities;
+    private LocalDate date;
+    private Subject subject;
+}
