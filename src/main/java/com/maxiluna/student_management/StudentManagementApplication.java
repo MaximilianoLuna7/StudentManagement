e87d1e3bd987b1196b68c09b4e2c66e197b3f768
Maximiliano Luna <maxiespn21@gmail.com>
2025-01-31 22:13:14 -0300

    refactor(structure): rename base package to com.maxiluna.student_management
    - Fix typo in base package name (from studenmanagement to student_management).
    - Update package declarations in all affected classes.

--- src/main/java/com/maxiluna/student_management/StudentManagementApplication.java
+++ src/main/java/com/maxiluna/student_management/StudentManagementApplication.java
@@ -1,4 +1,4 @@
-package com.studenmanagement;
+package com.maxiluna.student_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/maxiluna/student_management/StudentManagementApplicationTests.java
+++ src/test/java/com/maxiluna/student_management/StudentManagementApplicationTests.java
@@ -1,4 +1,4 @@
-package com.studenmanagement;
+package com.maxiluna.student_management;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
