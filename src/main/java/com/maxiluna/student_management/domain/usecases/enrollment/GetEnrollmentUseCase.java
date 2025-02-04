3a2c20ca00edc29d871394c5c7bbd627d18559af
Maximiliano Luna <maxiespn21@gmail.com>
2025-02-03 23:25:58 -0300

    refactor(domain): remove GetEnrollmentUseCase
    - Remove GetEnrollmentUseCase as it does not align with system requirements.
    - The use case is no longer needed due to changes in business logic.

--- src/main/java/com/maxiluna/student_management/domain/usecases/enrollment/GetEnrollmentUseCase.java
+++
@@ -1,7 +0,0 @@
-package com.maxiluna.student_management.domain.usecases.enrollment;
-
-import com.maxiluna.student_management.domain.models.Enrollment;
-
-public interface GetEnrollmentUseCase {
-    Enrollment execute(Long enrollmentId);
-}
